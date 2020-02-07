c484ba18358975b95da9e71bb23e95e5033d254a
Maxi Merrillees <maxi.merrillees@gmail.com>
2020-02-07 15:54:38 +1100

    Create functionality with pure django and templates

---
+++ pure_django_project/products/views.py
@@ -0,0 +1,12 @@
+from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
+from .models import Product, Manufacturer
+
+# Create your views here.
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'products/product_detail.html'
+
+class ProductListView(ListView):
+    model = Product
+    template_name = 'products/products_list.html'
